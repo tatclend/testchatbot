7682b9c94bb25763ee86c821752383ccaaea572e
Tate Clendening <Tate.Clendening@trimedx.com>
2023-07-11 15:09:06 -0400

    commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,12 +34,12 @@ if "chatbot" not in st.session_state:
         # load cookies from usercookies/<email>.json
         try:
             sign = Login(__email, None)
-            cookies = sign.loadCookiesFromDir()
+            cookies = sign.loadCookies()
         except:
             __pswd = os.getenv("HG_KEY")
             sign = Login(__email, __pswd)
             cookies = sign.login()
-            sign.saveCookiesToDir()
+            sign.saveCookies()
         
         st.session_state["chatbot"] = hugchat.ChatBot(cookies=cookies.get_dict())
 
